0598b8332be725aa452f7230f0259b3a1a1d2717
UsaniGee <usani3121@gmail.com>
2024-12-08 04:59:35 +0100

    fix image issue

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -28,7 +28,7 @@ const Map = () => {
             </div>
 
             <iframe 
-            className='w-[100%] h-[1100px] p-5'
+            className='w-[100%] h-screen p-5'
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7951.326158152643!2d6.990746784767743!3d4.827788016722936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1069cf40a3823d37%3A0x143a766549e649b3!2sPort%20Harcourt%20Polo%20Club!5e0!3m2!1sen!2sng!4v1729269955346!5m2!1sen!2sng" 
             allowfullscreen="" 
             loading="lazy" 
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,42 +1,43 @@
 import React from 'react'
+import image from "../../../public/assets/group 49.png"
 
 const Sidebar = () => {
   return (
-    <div className='pt-[55px] px-[46px] bg-white w-[500px] h-screen shadow-2xl'>
+    <div className='pt-[55px] px-[46px] bg-white w-[500px]  '>
 
       {/* Logo */}
       <div>
-        <img src="../../../public/assets/Group 49.png" alt="Logo" />
+        <img src="assets/Group 49.png" alt="Logo" />
       </div>
 
       {/* Sidebar Items */}
       <div className='h-[408px] flex flex-col justify-between mt-20'>
         <div className='flex gap-5 items-center'>
-        <img src="../../../public/assets/home.png" alt="" />
+        <img src="assets/home.png" alt="" />
           <span>Home</span>
         </div>
         <div className='flex gap-5 items-center'>
-        <img src="../../../public/assets/account.png" alt="" />
+        <img src="public/assets/account.png" alt="" />
           <span>Driver</span>
         </div>
         <div className='flex gap-5 items-center'>
-        <img src="../../../public/assets/group.png" alt="" />
+        <img src="public/assets/group.png" alt="" />
           <span>Cars</span>
         </div>
         <div className='flex gap-5 items-center'>
-        <img src="../../../public/assets/calendar-2.png" alt="" />
+        <img src="assets/calendar-2.png" alt="" />
           <span>Schedule</span>
         </div>
         <div className='flex gap-5 items-center'>
-        <img src="../../../public/assets/data.svg" alt="" />
+        <img src="public/assets/data.svg" alt="" />
           <span>Backup</span>
         </div>
         <div className='flex gap-5 items-center'>
-          <img src="../../../public/assets/setting.png" alt="" />
+          <img src="public/assets/setting.png" alt="" />
           <span>Settings</span>
         </div>
         <div className='flex gap-5 items-center'>
-        <img src="../../../public/assets/shut-down.png" alt="" />
+        <img src="assets/shut-down.png" alt="" />
           <span>Log out</span>
         </div>
       </div>
